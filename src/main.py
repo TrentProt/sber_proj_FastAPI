65a0c944acab1acc3316f83afda4a18c6566e764
Данила Рябинин <ryabinin.danila355@mail.ru>
2025-04-14 13:21:45 +0300

    no more create all on start

--- src/main.py
+++ src/main.py
@@ -4,18 +4,15 @@ import uvicorn
 
 from fastapi import FastAPI
 
-
-from src.core.models import db_helper, Base
 from src.core.models.users import Users
-
 from src.users.views import router as users_router
 
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    # async with db_helper.engine.begin() as conn:
+    #     await conn.run_sync(Base.metadata.create_all)
     yield
 
 
